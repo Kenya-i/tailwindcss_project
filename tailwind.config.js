3005d773c8347dd2e0807107dc2e196c6ee5138d
Kenya Ishizuka <ken8116@icloud.com>
2022-10-20 00:33:50 +0900

    ナビゲーションバー1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {
-          100: "#ebf8ff",
-          300: "#90cdf4",
-          500: "#4299e1"
-        }
+        body : '#050505',
+      
       }
     },
     fontFamily: {
